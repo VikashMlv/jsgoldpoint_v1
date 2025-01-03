eff71af1a54e387119f475247896a98c8858a5fc
VikashMlv <82767534+VikashMlv@users.noreply.github.com>
2025-01-03 23:36:19 +0530

    changes in all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from 'src/pages/about/about.component';
+import { ContactComponent } from 'src/pages/contact/contact.component';
+import { HomeComponent } from 'src/pages/home/home.component';
 
-const routes: Routes = [];
+var routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },  // Default route
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: '**', redirectTo: '', pathMatch: 'full' }  // Wildcard route for 404 page
+    ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from 'src/pages/home/home.component';
+import { AboutComponent } from 'src/pages/about/about.component';
+import { ContactComponent } from 'src/pages/contact/contact.component';
+import { FooterComponent } from 'src/pages/footer/footer.component';
+import { HeaderComponent } from 'src/pages/header/header.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }  // Provide HashLocationStrategy
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
